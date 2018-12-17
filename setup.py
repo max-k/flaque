ff10efe25339ab5249a56722aa9e4bcb4c89e166
Thomas Sarboni <max-k@post.com>
2018-12-17 23:33:16 +0100

    Fix requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
+with open('requirements-test.txt') as test_require:
+    TEST_REQUIREMENTS = test_require.readlines()
+
 setup(
     name='flaque',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
+    setup_require=[
+        'pytest-runner'
+    ],
     install_requires=[
-        'Click',
+        'click',
+    ],
+    test_require=[
+        TEST_REQUIREMENTS
     ],
     entry_points='''
         [console_scripts]
