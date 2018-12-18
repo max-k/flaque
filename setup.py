c06efdd39da2e794ce9f1d957bc51744393a4bf9
Thomas Sarboni <max-k@post.com>
2018-12-18 01:02:11 +0100

    Fix tests ans tests_require

--- setup.py
+++ setup.py
@@ -3,21 +3,19 @@
 from setuptools import setup, find_packages
 
 TEST_REQ = ['pytest']
-EXTRAS_REQ = {'test': TEST_REQ}
 
 setup(
     name='flaque',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    setup_require=[
+    setup_requires=[
         'pytest-runner'
     ],
     install_requires=[
         'click',
     ],
-    test_require=TEST_REQ,
-    extra_require=EXTRAS_REQ,
+    tests_require=TEST_REQ,
     entry_points='''
         [console_scripts]
         flaque=flaque.__main__:main
