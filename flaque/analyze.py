4a51d1392a03a8b8926945a041496fef9e95a521
Thomas Sarboni <max-k@post.com>
2018-12-21 14:52:33 +0100

    One more step

--- flaque/analyze.py
+++ flaque/analyze.py
@@ -12,11 +12,14 @@ from .metatags import AudioFile
 def analyzer(directory: Directory, force_type: str) -> Dict[str, Album]:
     albums: Dict[str, Album] = []
     for _file in [x for x in directory.files if x.format != "unsupported"]:
-        try:
-            data: AudioFile = AudioFile(_file)
-        except MutagenError:
-            print("Error loading tags for {}".format())
-        if 'album' in data:
-            if data['album'] not in albums:
-                albums[data['album']] = Album(data['album'])
+        if force_type == 'track' or _file.size < 150:
+            try:
+                data: AudioFile = AudioFile(_file)
+            except MutagenError:
+                print("Error loading tags for {}".format())
+            if 'album' in data:
+                if data['album'] not in albums:
+                    albums[data['album']] = Album(data['album'])
+        elif force_type == 'disc' or _file.size >= 150:
+            pass
     return albums
